0759c1d3381b4e33b6f180f10d956ce83b18234a
SamuelQuetin <samquetinuel@gmail.com>
2023-02-22 15:49:18 +0100

    Initialisation de kbartwebservices

---
+++ kbart-webservices/src/main/java/fr/abes/convergence/kbartws/KbartWebservicesApplication.java
@@ -0,0 +1,22 @@
+package fr.abes.convergence.kbartws;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
+
+
+@SpringBootApplication
+public class KbartWebservicesApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Paris"));   // It will set UTC timezone
+        SpringApplication.run(KbartWebservicesApplication.class, args);
+    }
+    @Override
+    public void run(String... args) {
+        System.out.println("HELLO WORLD");
+    }
+
+}
