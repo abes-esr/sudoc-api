073d6656ff6438fb0c7d2d69a75c602275093445
EryneKL <97091460+EryneKL@users.noreply.github.com>
2023-03-02 10:16:02 +0100

    FEAT :      - modification du TU SudocControllerTest.java

--- sudoc-webservices/src/test/java/fr/abes/convergence/sudocws/controller/SudocControllerTest.java
+++ sudoc-webservices/src/test/java/fr/abes/convergence/sudocws/controller/SudocControllerTest.java
@@ -1,12 +1,50 @@
 package fr.abes.convergence.sudocws.controller;
 
+import org.assertj.core.util.Lists;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest(classes = {SudocController.class})
+@ContextConfiguration(classes = {})
 class SudocControllerTest {
+
+    @Autowired
+    WebApplicationContext context;
+
+    @InjectMocks
+    SudocController controller;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+        this.mockMvc = MockMvcBuilders
+                .standaloneSetup(context.getBean(SudocController.class))
+//                .setControllerAdvice(new ExceptionControllerHandler())
+                .build();
+    }
 
     @Test
     void datToPpn() {
     }
 }
+
