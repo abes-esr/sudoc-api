1ed246f11bf18d73e8e4c3e6c229bea554e9e51d
EryneKL <97091460+EryneKL@users.noreply.github.com>
2023-03-02 10:10:22 +0100

    FEAT :      - modification du pom.xml      - ajout de la classe SudocControllerTest.java      - ajout de la classe SudocServiceTest.java

---
+++ sudoc-webservices/src/test/java/fr/abes/convergence/sudocws/controller/SudocControllerTest.java
@@ -0,0 +1,12 @@
+package fr.abes.convergence.sudocws.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SudocControllerTest {
+
+    @Test
+    void datToPpn() {
+    }
+}
---
+++ sudoc-webservices/src/test/java/fr/abes/convergence/sudocws/service/SudocServiceTest.java
@@ -0,0 +1,12 @@
+package fr.abes.convergence.sudocws.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SudocServiceTest {
+
+    @Test
+    void getPpnFromDat() {
+    }
+}
