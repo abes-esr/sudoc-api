2d416ab3f9d0332d508b76066b4745093e20361e
EryneKL <97091460+EryneKL@users.noreply.github.com>
2023-03-03 15:09:06 +0100

    FEAT :      - modification du TU SudocServiceTest.java

--- sudoc-webservices/src/test/java/fr/abes/convergence/sudocws/service/SudocServiceTest.java
+++ sudoc-webservices/src/test/java/fr/abes/convergence/sudocws/service/SudocServiceTest.java
@@ -4,19 +4,15 @@ import fr.abes.cbs.exception.CBSException;
 import fr.abes.cbs.process.ProcessCBS;
 import fr.abes.convergence.sudocws.exception.ControllerExceptionHandler;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest(classes = {SudocService.class, ControllerExceptionHandler.class})
@@ -25,50 +21,39 @@ class SudocServiceTest {
     @Autowired
     WebApplicationContext context;
 
-    MockMvc mockMvc;
-
     @MockBean
     ProcessCBS cbs;
 
     @Autowired
     SudocService service;
 
-    @BeforeEach
-    void init() {
-        MockitoAnnotations.openMocks(this);
-        this.mockMvc = MockMvcBuilders
-                .standaloneSetup(context.getBean(SudocService.class))
-                .setControllerAdvice(ControllerExceptionHandler.class)
-                .build();
-    }
-
     @Test
-    @DisplayName("getPpnFromDat")
-    void getPpnFromDat() throws CBSException {
+    @DisplayName("getPpnFromDatWithAnneeAuteurTitre")
+    void getPpnFromDatWithAnneeAuteurTitre() throws CBSException {
 
         Integer annee = 2008;
         String auteur = "Auteur";
         String titre = "Ours";
 
-        java.lang.String serveur = "";
-        java.lang.String port = "";
-        java.lang.String login = "";
-        java.lang.String passwd = "";
+        Mockito.when(cbs.search("tno t ; tdo b ; apu " + annee + " ; che aut " + auteur + " et mti " + titre)).thenReturn("123456789");
+        Mockito.when(cbs.getListePpn()).thenReturn(new StringBuilder("123456789;987654321"));
+        Mockito.when(cbs.getNbNotices()).thenReturn(2);
 
-        List<String> ppnList = new ArrayList<>();
-        ppnList.add("123456789");
+        List<String> result = service.getPpnFromDat(annee, auteur, titre);
 
-//        Mockito.doReturn("123456789").when(cbs.search("tno t ; tdo b ; apu " + annee + " ; che aut " + auteur + " et mti " + titre));
-//        Mockito.when(cbs.search("tno t ; tdo b ; apu " + annee + " ; che aut " + auteur + " et mti " + titre)).thenReturn()
+        Assertions.assertEquals( "123456789", result.get(0));
+    }
 
+    @Test
+    @DisplayName("getPpnFromDatAllIsNull")
+    void getPpnFromDatAllIsNull() throws CBSException {
 
-//        Mockito.doNothing().when(cbs.authenticate(serveur, port, login, passwd));
-//        Mockito.doNothing().when(cbs.search("tno t ; tdo b ; apu " + annee + " ; che aut " + auteur + " et mti " + titre));
-//        Mockito.when(cbs.getListePpn()).thenReturn(new StringBuilder(ppnList.toString()));
-//        Mockito.when(cbs.getNbNotices()).thenReturn(ppnList.size());
-//
-//        List<String> result = service.getPpnFromDat(annee, auteur, titre);
-//
-//        Assertions.assertEquals( "123456789", result.toString());
+        Integer annee = null;
+        String auteur = null;
+        String titre = null;
+
+        List<String> result = service.getPpnFromDat(annee, auteur, titre);
+
+        Assertions.assertEquals( 0, result.size());
     }
 }
