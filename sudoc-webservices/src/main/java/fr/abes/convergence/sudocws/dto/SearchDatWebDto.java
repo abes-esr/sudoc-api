878146fcb95fdbe4573fa9a2ed3e93530e34b54a
maraval <maraval@abes.fr>
2023-03-13 15:44:07 +0100

    CDE-61 : Fix : ajout nom des champ json dto en entrée

--- sudoc-webservices/src/main/java/fr/abes/convergence/sudocws/dto/SearchDatWebDto.java
+++ sudoc-webservices/src/main/java/fr/abes/convergence/sudocws/dto/SearchDatWebDto.java
@@ -1,10 +1,14 @@
 package fr.abes.convergence.sudocws.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class SearchDatWebDto {
+    @JsonProperty("date")
     private Integer date;
+    @JsonProperty("auteur")
     private String auteur;
+    @JsonProperty("titre")
     private String titre;
 }
