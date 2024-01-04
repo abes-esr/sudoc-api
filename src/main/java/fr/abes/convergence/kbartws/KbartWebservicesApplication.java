512211e79c98bebfb16cf2ba4e6ce25466fe6d00
EryneKL <97091460+EryneKL@users.noreply.github.com>
2024-01-04 15:08:39 +0100

    FEAT : CDE-96-Vérification du provider sur dat2ppn      - fusion des deux modules

---
+++ src/main/java/fr/abes/convergence/kbartws/KbartWebservicesApplication.java
@@ -0,0 +1,29 @@
+package fr.abes.convergence.kbartws;
+
+import fr.abes.cbs.process.ProcessCBS;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.TimeZone;
+
+
+@SpringBootApplication
+public class KbartWebservicesApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        System.out.println("sudoc-webservices");
+        TimeZone.setDefault(TimeZone.getTimeZone("Europe/Paris"));   // It will set UTC timezone
+        SpringApplication.run(KbartWebservicesApplication.class, args);
+    }
+    @Override
+    public void run(String... args) {
+    }
+
+    @Bean
+    public ProcessCBS initCbs() {
+        return new ProcessCBS();
+    }
+
+}
