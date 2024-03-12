7cef2951f93a4cc41ccf6aaedbb01a761ad518b4
SamuelQuetin <quetin@abes.fr>
2024-03-12 10:37:25 +0100

    Ajout constructeur a partir d'une notice Fix gestion type support Fix TU

--- src/main/java/fr/abes/sudoc/dto/PpnWithTypeWebDto.java
+++ src/main/java/fr/abes/sudoc/dto/PpnWithTypeWebDto.java
@@ -1,5 +1,7 @@
 package fr.abes.sudoc.dto;
 
+import fr.abes.sudoc.entity.notice.NoticeXml;
+import fr.abes.sudoc.exception.ZoneNotFoundException;
 import fr.abes.sudoc.utils.TYPE_DOCUMENT;
 import fr.abes.sudoc.utils.TYPE_SUPPORT;
 import lombok.AllArgsConstructor;
@@ -12,4 +14,11 @@ public class PpnWithTypeWebDto {
     TYPE_SUPPORT typeSupport;
     TYPE_DOCUMENT typeDocument;
     Boolean providerPresent;
+
+    public PpnWithTypeWebDto(NoticeXml noticeXml, Boolean providerPresent) throws ZoneNotFoundException {
+        this.ppn = noticeXml.getPpn();
+        this.typeSupport = noticeXml.getTypeSupport();
+        this.typeDocument = noticeXml.getTypeDocument();
+        this.providerPresent = providerPresent;
+    }
 }
