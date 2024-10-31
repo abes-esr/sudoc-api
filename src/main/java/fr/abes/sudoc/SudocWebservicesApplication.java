9ba8db7301308a11efd084c7518bcb486e1c531d
pierre-maraval <pierre.maraval@gmail.com>
2024-10-31 07:55:44 +0100

    FIX : Suppression du bean sur AccesCbs et déclaration en variable locale

--- src/main/java/fr/abes/sudoc/SudocWebservicesApplication.java
+++ src/main/java/fr/abes/sudoc/SudocWebservicesApplication.java
@@ -1,10 +1,8 @@
 package fr.abes.sudoc;
 
-import fr.abes.cbs.process.ProcessCBS;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 import java.util.TimeZone;
 
@@ -21,10 +19,4 @@ public class SudocWebservicesApplication implements CommandLineRunner {
     @Override
     public void run(String... args) {
     }
-
-    @Bean
-    public ProcessCBS initCbs() {
-        return new ProcessCBS();
-    }
-
 }
