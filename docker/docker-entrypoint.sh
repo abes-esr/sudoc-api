62ac91df5d042f95fc8069a470cbd8b8efac6ca1
pierre-maraval <pierre.maraval@gmail.com>
2024-11-28 15:25:31 +0100

    FEAT : ajout docker-entrypoint.sh

---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,2 @@
+export HOSTNAME=${HOSTNAME}
+java -XX:MaxRAMPercentage=75 -XX:+UseG1GC -XX:ConcGCThreads=5 -XX:+ExitOnOutOfMemoryError -XX:MaxGCPauseMillis=100 -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=9090 -Dcom.sun.management.jmxremote.rmi.port=9090 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.local.only=false -Djava.rmi.server.hostname=${HOSTNAME} -jar /app/sudoc.jar
