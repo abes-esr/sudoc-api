fd8e6341dbb3d3d1022e536b5aaa1c55d1cdb45f
SamuelQuetin <quetin@abes.fr>
2025-05-06 11:19:23 +0200

    CDE-492 Add method for deleting stopEnglishWords

--- src/main/java/fr/abes/sudoc/utils/Utilitaire.java
+++ src/main/java/fr/abes/sudoc/utils/Utilitaire.java
@@ -96,7 +96,7 @@ public class Utilitaire {
 
     public static String formatString(String chaine) {
         if (chaine != null && !chaine.isEmpty()) {
-            return ajoutNearBetweenWords(banalisationReservedWords(suppCaracters(chaine)));
+            return ajoutNearBetweenWords(banalisationReservedWords(suppStopWords(suppCaracters(chaine))));
         }
         return chaine;
     }
@@ -129,6 +129,13 @@ public class Utilitaire {
         return String.join(" NEAR ", words);
     }
 
+    private static String suppStopWords(String chaine) {
+        List<String> stopWords = Arrays.asList("a","all","almost","also","although","an","and","any","are","as","at","be","because","been","both","but","by","can","could","d","did","do","does","either","for","from","had","has","have","having","he","her","here","hers","him","his","how","however","i","if","in","into","is","it","its","just","ll","me","might","Mr","Mrs","Ms","my","no","non","nor","not","of","on","one","only","onto","or","our","ours","s","shall","she","should","since","so","some","still","such","t","than","that","the","their","them","then","there","therefore","these","they","this","those","though","through","thus","to","too","until","ve","very","was","we","were","what","when","where","whether","which","while","who","whose","why","will","with","would","yet","you","your","yours");
+
+        String regex = "\\b(" + String.join("|", stopWords) + ")\\b";
+
+        return chaine.toLowerCase().replaceAll(regex, "");
+    }
 
 }
 
