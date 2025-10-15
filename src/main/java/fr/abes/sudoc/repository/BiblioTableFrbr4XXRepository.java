4fca922dced9815437239a1e35b0d66f87dbb624
pierre-maraval <pierre.maraval@gmail.com>
2025-10-15 11:12:01 +0200

    Fix: deleted log4j properties

--- src/main/java/fr/abes/sudoc/repository/BiblioTableFrbr4XXRepository.java
+++ src/main/java/fr/abes/sudoc/repository/BiblioTableFrbr4XXRepository.java
@@ -1,11 +1,12 @@
 package fr.abes.sudoc.repository;
 
 import fr.abes.sudoc.entity.BiblioTableFrbr4XX;
-import fr.abes.sudoc.utils.ExecutionTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface BiblioTableFrbr4XXRepository extends JpaRepository<BiblioTableFrbr4XX, Integer> {
 
     List<BiblioTableFrbr4XX> findAllByTagAndDatas(String tag, String datas);
